ce241abb0c1fac97c4acbc709a57df3a0cd21d15
Pascal Simon <simon@puzzle.ch>
2019-12-07 21:47:37 +0100

    fix ember test, switch to chromium

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -4,7 +4,7 @@ import { inject as service } from '@ember/service';
 export default Component.extend({
   store: service(),
 
-  cams: [],
+  cams: null,
 
   init() {
     this._super(...arguments);
